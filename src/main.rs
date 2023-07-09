8d33b6b340c722ee8d0aff8da8f21f5c5778f357
Graham Brooks <graham@grahambrooks.com>
2023-07-09 11:47:26 -0600

    Added new feature to the command line app dev-shell

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,20 @@ use clap::Parser;
 
 use crate::client::GPT_3_5_TURBO;
 use crate::completion::{CompletionMessage, StreamedResponse};
+use crate::messages::{CODE_TEMPLATE, DEFAULT_TEMPLATE, SHELL_TEMPLATE};
 
 mod client;
 mod completion;
+mod context;
+mod messages;
 
 #[derive(Parser, Debug)]
 #[command(author = "Graham Brooks", version = "0.1", about = "Shell for AI assisted development", long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    summarize: bool,
+    #[arg(long, help = "generate a command line give the prompt and the option to run the command")]
+    command: bool,
+    #[arg(long, help = "generate source code in response to the prompt")]
+    code: bool,
     prompt: Vec<String>,
 }
 
@@ -41,47 +46,95 @@ async fn main() {
         return;
     }
 
-    if args.summarize {
-        println!("Summarize mode");
-    }
-
     let client = client::Client::new(openapi_key.unwrap());
 
-    fn callback(response: &StreamedResponse) {
-        response.choices.iter().for_each(|c| {
-            let content = &c.delta.content;
-            match content {
-                Some(content) => print!("{}", content),
-                None => (),
-            }
-        });
+    if args.command {
+        command(client, args.prompt).await;
+        return;
     }
 
-    let mut prompt = args.prompt.join(" ").to_string();
+    if args.code {
+        code(client, args.prompt).await;
+        return;
+    }
 
+    default(client, args.prompt).await;
+}
+
+async fn command(client: client::Client, elements: Vec<String>) {
+    let mut prompt = elements.join(" ").to_string();
+    prompt.push_str(read_stdin().as_str());
+
+    let messages = SHELL_TEMPLATE.expand(vec![
+        ("shell", context::shell().as_str()),
+        ("os", context::os().as_str()),
+        ("request", prompt.as_str()),
+    ]).unwrap();
+
+    make_request(client, messages).await;
+}
+
+async fn code(client: client::Client, elements: Vec<String>) {
+    let mut prompt = elements.join(" ").to_string();
+    prompt.push_str(read_stdin().as_str());
+
+    let messages = CODE_TEMPLATE.expand(vec![
+        ("request", prompt.as_str()),
+    ]).unwrap();
+
+    make_request(client, messages).await;
+}
+
+async fn default(client: client::Client, elements: Vec<String>) {
+    let mut prompt = elements.join(" ").to_string();
+    prompt.push_str(read_stdin().as_str());
+
+    let messages = DEFAULT_TEMPLATE.expand(vec![
+        ("shell", context::shell().as_str()),
+        ("os", context::os().as_str()),
+        ("request", prompt.as_str()),
+    ]).unwrap();
+
+    make_request(client, messages).await;
+}
+
+
+fn read_stdin() -> String {
+    let mut result: String = "".to_string();
     if !atty::is(atty::Stream::Stdin) {
         let mut buffer: String = "".to_string();
         match io::stdin().read_to_string(&mut buffer) {
             Ok(_) => {
-                prompt.push_str("\n");
-                prompt.push_str(buffer.as_str());
+                result.push('\n');
+                result.push_str(buffer.as_str());
             }
             Err(error) => {
                 println!("Failed to read from stdin: {}", error);
+                return "".to_string();
             }
         }
     }
+    result.to_string()
+}
 
+async fn make_request(client: client::Client, prompt: String) {
     completion::request([
-        // &CompletionMessage::from_str("system", "you are a helpful assistant"),
         &CompletionMessage::from_str("user", prompt.as_str())
     ].to_vec())
         .model(GPT_3_5_TURBO)
         .temperature(0.5)
         .stream()
         .call_streamed_response(client, callback)
-// .call(client, |s| println!("callback received {}", s))
-        .await
+        .await;
 }
 
 
+fn callback(response: &StreamedResponse) {
+    response.choices.iter().for_each(|c| {
+        let content = &c.delta.content;
+        match content {
+            Some(content) => print!("{}", content),
+            None => (),
+        }
+    });
+}
